40d7a137b483dce2740f21b36746d9382e19495e
Deogle <deogle20@gmail.com>
2017-10-06 13:11:11 -0400

    Added Game Files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject mainCam;
+    public GameObject unit;
+    public Player playerUnit;
+
+    private MapMaker map;
+
+	// Use this for initialization
+	void Start () {
+        map = GetComponent<MapMaker>();
+        map.BoardSetup();
+        mainCam.transform.position = map.GetCenter(); //Fix camera on the center of the map
+        map.SpawnUnit(unit, 3, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D hitCol = Physics2D.OverlapPoint(mousePos);
+
+            if (hitCol)
+            {
+                if(hitCol.gameObject.tag == "Player")
+                {
+                    playerUnit = hitCol.GetComponent<Player>();
+                    Debug.Log("Clicked Player Unit");
+                    playerUnit.Select();
+                }
+                else if (hitCol.gameObject.tag == "Tile")
+                {
+                    Debug.Log("Clicked Wood Panel!");
+                    
+                }
+            }
+        }        
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject {
+
+    public int moveRange;
+    public int damage;
+    public int health;
+    public int xp;
+    public int speed;
+    public bool isSelected = false;
+
+    private Color thisColor;
+
+    public int x, y;
+
+    public override void Start()
+    {
+        base.Start();
+        thisColor = this.GetComponent<SpriteRenderer>().material.color;
+    }
+
+    public void Select()
+    {
+        isSelected = !isSelected;
+        if (isSelected)
+            UnitSelected();
+        else
+            UnitDeselected();
+    }
+
+    private void UnitSelected()
+    {
+        //TODO Highlight unit and list possible moves
+        SpriteRenderer mat = this.GetComponent<SpriteRenderer>();
+        Color newColor = new Color(250,255,0);
+        mat.material.SetColor("_Color", newColor);
+    }
+
+    private void UnitDeselected()
+    {
+        SpriteRenderer mat = this.GetComponent<SpriteRenderer>();
+        mat.material.SetColor("_Color", thisColor);
+    }
+}
